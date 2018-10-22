d238f71f040b2eaba59ee4bd09a3037a7814ef4b
Sam Meech-Ward <sam@meech-ward.me>
2018-10-22 16:16:38 -0700

    Update motor pins

--- carControls.js
+++ carControls.js
@@ -1,9 +1,10 @@
 const Motor = require("./Motor");
 
 const motors = {   
-  back: Object.create(Motor).setPins(17, 6, 22),
-  front: Object.create(Motor).setPins(25, 13, 18),
-  steering: Object.create(Motor).setPins(12, 23, 16),
+  steering2: Object.create(Motor).setPins(17, 6, 22), // 2
+  front: Object.create(Motor).setPins(25, 13, 18), // 4
+  back: Object.create(Motor).setPins(12, 23, 16), // 3
+  steering: Object.create(Motor).setPins(5, 27, 24) // 1
 };
 
 function enable() {
@@ -32,13 +33,16 @@ module.exports = {
   right: () => {
     console.log("right");
     motors.steering.forward();
+    motors.steering2.forward();
   },
   left: () => {
     console.log("left");
     motors.steering.backward();
+    motors.steering2.backward();
   },
   stopSteering: () => {
     console.log("stop steering");
     motors.steering.stop();
+    motors.steering2.stop();
   },
 }
